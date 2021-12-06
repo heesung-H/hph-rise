c8f1ed773e85fe9c6fdcab6d16104a0c00ae56c7
park-sh1 <sung90@naver.com>
2021-12-06 23:18:16 +0900

    게시판 목록개발중

--- src/main/java/com/hph/rise/controller/BoardController.java
+++ src/main/java/com/hph/rise/controller/BoardController.java
@@ -1,15 +1,33 @@
 package com.hph.rise.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.model.IModel;
 
 @RestController
 @RequestMapping("/board")
 public class BoardController {
 
+    /* 공통게시판 */
     @GetMapping("/list")
-    public String boardList() {
-        return "board/list";
+    public String boardList(Model model) {
+        // 게시판 데이터 불러오기 - Model model
+
+        //model.addAttribute("boardlist", )
+
+        return "/board/list";
     }
+
+    /* 게시글 등록화면으로 이동 */
+
+    /* 게시글 등록 */
+    /* 게시글 수정 */
+    /* 게시글 삭제 */
+
+
+
+
 }
