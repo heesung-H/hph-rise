7769076d27746ecdf335eff44a2e65a82d8b459c
park-sh1 <sung90@naver.com>
2021-12-08 21:56:02 +0900

    로그인 화면 진행

---
+++ src/main/java/com/hph/rise/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.hph.rise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+    //@Autowired
+    //LoginService loginService;
+
+    /**
+     * <p>로그인 화면으로</p>
+     * @return
+     */
+    @GetMapping("/")
+    public String login(){
+        return "/login/login.html";
+    }
+
+    /**
+     * <p>회원가입 화면으로</p>
+     * @return
+     */
+    @GetMapping("/signUp")
+    public String signUp() { return "/login/signUp.html";}
+
+    //로그인 처리
+
+
+    // 로그아웃 처리
+
+    // id찾기 화면으로
+
+    // pw찾기 화면으로
+
+}
--- src/main/java/com/hph/rise/controller/NoticeController.java
+++ src/main/java/com/hph/rise/controller/NoticeController.java
@@ -1,17 +1,18 @@
 package com.hph.rise.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/notice")
+@Controller
+//@RequestMapping("/notice")
 public class NoticeController {
 
-    @GetMapping("/list")
+    @GetMapping("/notice/list")
     public String noticeList() {
 
-        return "/notice/list.html";
+        return "notice/list";
     }
 }
